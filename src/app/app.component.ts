d866e21ad189430e64179a450ab5a992fb78c794
David Strahm <david.strahm@lambda-it.ch>
2023-07-14 12:29:23 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'workers-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'workers';
+}
